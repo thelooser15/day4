f30316e7a0b1489bd053513334c61365536b1ee6
pavan dharavath <97276341+thelooser15@users.noreply.github.com>
2025-08-02 15:11:14 +0530

    Add files via upload

---
+++ Abstract/Abstract/Bhavadeesh.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractExample1
+{
+    internal class Bhavadeesh : Training
+    {
+        public override void Email()
+        {
+            Console.WriteLine("Hi My Email is nbr@gmail.com");
+        }
+
+        public override void Name()
+        {
+            Console.WriteLine("Hi I am Bhavadeesh...");
+        }
+    }
+}
